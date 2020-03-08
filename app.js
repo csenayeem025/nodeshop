691f3684d1f199cc98003ca751bfc493c4310137
Nayeem, Khurshed Alam <csenayeem025@gmail.com>
2020-03-08 10:27:06 +0000

    server changes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 8888
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const port = process.env.PORT || 8880;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// viewed at http://localhost:3000
+app.get("/", function(req, res) {
+  // this does not exist
+  fs.createReadStream("index.html");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
